88154b6b038a7b40827fc63921b5b706f845067a
Maxim Volkov <maksim_volkov1@epam.com>
2022-09-10 18:26:54 +0300

    shows division by zero depends on data type

--- Program.cs
+++ Program.cs
@@ -1,10 +1,6 @@
-﻿var a = 1;
-var b = GetDenominator(0);
+﻿var a = 1.0;
+double b = 0.0;
 
 Console.WriteLine(a/b);
 Console.WriteLine("Completed");
 
-int GetDenominator(int v)
-{
-    return v;
-}
