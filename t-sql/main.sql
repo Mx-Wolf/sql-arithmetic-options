e22ab2a38688e502753f7e91d619dda146379bfc
Maxim Volkov <maksim_volkov1@epam.com>
2022-09-10 17:33:57 +0300

    shows sql try-catch prevents next statement

--- t-sql/main.sql
+++ t-sql/main.sql
@@ -1,8 +1,17 @@
 declare @a int = 1,
 @b int = 0;
 
-select
-  @a / @b [ratio];
+begin TRY
 
-select
-  'Completed' [message];
+  select
+    @a / @b [ratio];
+
+  select
+    'Completed' [message];
+
+end TRY begin CATCH
+  select 
+    ERROR_LINE() [ERROR_LINE],
+    ERROR_NUMBER() [ERROR_NUMBER],
+    ERROR_MESSAGE() [ERROR_MESSAGE]
+end CATCH
