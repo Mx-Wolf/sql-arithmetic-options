314ea083d93000d98474240f1540f121a7a404f3
Maxim Volkov <maksim_volkov1@epam.com>
2022-09-14 13:00:26 +0300

    shows the negative zero

--- Program.cs
+++ Program.cs
@@ -4,3 +4,10 @@ double b = 0.0;
 Console.WriteLine(a/b);
 Console.WriteLine("Completed");
 
+double negativeZero;
+negativeZero = GetValue(1);
+
+Console.WriteLine(Double.IsNegativeInfinity(1/negativeZero)?"equals":"different");
+
+double GetValue(double factor)=>factor/Double.NegativeInfinity;
+
